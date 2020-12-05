660b65272d58effd14dafda3255f4d15b71fa444
FUYUHSUAN <ramy1231863@gmail.com>
2020-12-05 14:33:22 +0800

    add

--- homework/test2.js
+++ homework/test2.js
@@ -1,6 +1,6 @@
 function getValue(){
-    var x = document.getElementById("loginForm");
-    var userfill = x.elements[0].value;
+    var x = document.getElementById("search");
+    var userfill = x.value;
     userfill=userfill.replace('""','') //將引號取代成空格
     console.log(userfill);
 }
